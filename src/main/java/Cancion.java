565a5fa45a92446b8e555ce3bf9a54782332d353
VictoRPiles <victorpilesdelgado@gmail.com>
2022-05-11 16:50:20 +0200

    Refactor para claridad del código.

--- src/main/java/Cancion.java
+++ src/main/java/Cancion.java
@@ -15,8 +15,13 @@ public class Cancion {
 
 	/**
 	 * Imprime la información de los metadatos.
+	 * <p>
+	 * Si tiene Id3v1Tag se imprime {@link #getTagsComunes(ID3v1)}.
+	 * <p>
+	 * Si tiene Id3v2Tag se imprime {@link #getTagsComunes(ID3v1)} + {@link #getTagsId3v2(ID3v2)}.
 	 *
 	 * @see #getTagsComunes(ID3v1)
+	 * @see #getTagsId3v2(ID3v2)
 	 * @see <a href="https://github.com/mpatric/mp3agic/blob/master/readme.md">Mp3agic Docs.</a>
 	 */
 	public void getTags() {
@@ -24,28 +29,15 @@ public class Cancion {
 			ID3v1 id3v1Tag = ficheroMp3.getId3v1Tag();
 			getTagsComunes(id3v1Tag);
 		}
-		/* Etiquetas exclusivas de Id3v2 */
 		if (ficheroMp3.hasId3v2Tag()) {
 			ID3v2 id3v2Tag = ficheroMp3.getId3v2Tag();
 			getTagsComunes(id3v2Tag);
-			System.out.println("Composer: " + id3v2Tag.getComposer());
-			System.out.println("Publisher: " + id3v2Tag.getPublisher());
-			System.out.println("Original artist: " + id3v2Tag.getOriginalArtist());
-			System.out.println("Album artist: " + id3v2Tag.getAlbumArtist());
-			System.out.println("Copyright: " + id3v2Tag.getCopyright());
-			System.out.println("URL: " + id3v2Tag.getUrl());
-			System.out.println("Encoder: " + id3v2Tag.getEncoder());
-			byte[] albumImageData = id3v2Tag.getAlbumImage();
-			if (albumImageData != null) {
-				System.out.println("Have album image data, length: " + albumImageData.length + " bytes");
-				System.out.println("Album image mime type: " + id3v2Tag.getAlbumImageMimeType());
-			}
+			getTagsId3v2(id3v2Tag);
 		}
 	}
 
 	/** Metadatos comunes en Id3v1 e Id3v2 */
 	private void getTagsComunes(ID3v1 id3v1Tag) {
-		/* Etiquetas comunes en todos los archivos */
 		System.out.println("Track: " + id3v1Tag.getTrack());
 		System.out.println("Artist: " + id3v1Tag.getArtist());
 		System.out.println("Title: " + id3v1Tag.getTitle());
@@ -53,5 +45,21 @@ public class Cancion {
 		System.out.println("Year: " + id3v1Tag.getYear());
 		System.out.println("Genre: " + id3v1Tag.getGenre() + " (" + id3v1Tag.getGenreDescription() + ")");
 		System.out.println("Comment: " + id3v1Tag.getComment());
+	}
+
+	/** Etiquetas exclusivas de Id3v2 */
+	private void getTagsId3v2(ID3v2 id3v2Tag) {
+		System.out.println("Composer: " + id3v2Tag.getComposer());
+		System.out.println("Publisher: " + id3v2Tag.getPublisher());
+		System.out.println("Original artist: " + id3v2Tag.getOriginalArtist());
+		System.out.println("Album artist: " + id3v2Tag.getAlbumArtist());
+		System.out.println("Copyright: " + id3v2Tag.getCopyright());
+		System.out.println("URL: " + id3v2Tag.getUrl());
+		System.out.println("Encoder: " + id3v2Tag.getEncoder());
+		byte[] albumImageData = id3v2Tag.getAlbumImage();
+		if (albumImageData != null) {
+			System.out.println("Have album image data, length: " + albumImageData.length + " bytes");
+			System.out.println("Album image mime type: " + id3v2Tag.getAlbumImageMimeType());
+		}
 	}
 }
